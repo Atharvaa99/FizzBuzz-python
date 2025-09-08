4b7d13a20ea80244dd89b77277729301d5f9daed
Atharvaa99 <atharvadhaigude@icloud.com>
2025-09-08 19:14:01 +0530

    Commit

---
+++ stage4.py
@@ -0,0 +1,19 @@
+def fizzBuzz(val):
+    if val % 3 == 0 and val % 7 == 0:
+        return "FizzBuzz"
+    elif val % 7 == 0:
+        return "Buzz"
+    elif  val % 3 == 0:
+        return "Fizz"
+    elif "3" in str(val):
+        return "Almost Fizz"
+    else:
+        return str(val)
+
+
+num = int(input())
+greeting = "Welcome to FizzBuzz!"
+print(greeting)
+for i in range(1,num+1):   
+    result = fizzBuzz(i)
+    print(result)
